a7e948b97e1078fa02fc9665caa3ca48b4a06600
Horvat Marta <tusya.horvat@gmail.com>
2026-07-02 04:39:27 +0300

    Implement car wash station

--- app/main.py
+++ app/main.py
@@ -47,7 +47,10 @@ class CarWashStation:
         return round(income, 1)
 
     def rate_service(self, rate: int) -> None:
-        self.average_rating = (
-            self.average_rating * self.count_of_ratings + rate
-        ) / (self.count_of_ratings + 1)
+        self.average_rating = round(
+            (
+                    self.average_rating * self.count_of_ratings + rate
+            ) / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
